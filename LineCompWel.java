4666456d0a35989b181e501ac254e330b6111f40
Purveshpund <purvesh.pund.work@gmail.com>
2021-09-20 15:27:05 +0530

    This gives a welcome messege to the project

---
+++ LineCompWel.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.LineComp;
+
+public class LineCompWel {
+    public static void main (String[] args){
+        // Welcome Messege
+        System.out.println("Hii there,This is Line Comparision Computation Program");
+    }
+}
+
+
+
+
+
+
